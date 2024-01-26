57e9ad23dc24ffb5f2b8fcb22aff1eed134d2b13
Alex Marriott <alex.marriott@buildcircle.co.uk>
2024-01-26 15:27:29 +0000

    update with superheroes content

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the hero and villain labels', () => {
+  render(<App  />);
+  const heroLabel = screen.getByText(/Hero/i);
+  const villainLabel = screen.getByText(/Villain/i);
+
+  expect(heroLabel).toBeInTheDocument();
+  expect(villainLabel).toBeInTheDocument();
 });
